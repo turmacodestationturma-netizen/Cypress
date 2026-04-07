6c32d6d795d533bc594184390311b4de5ed3a3e9
samucards <samuca_rds@yahoo.com.br>
2026-04-07 20:53:39 -0300

    ajuste d ecenarios

--- cypress/e2e/1-getting-started/FormularioAberturaContaPF.cy.js
+++ cypress/e2e/1-getting-started/FormularioAberturaContaPF.cy.js
@@ -26,21 +26,7 @@ describe('example to-do app', () => {
  
 
 })
-it('validar Formulario com cpf Invalido',()=>{
- cy.get('#onetrust-accept-btn-handler').click();
- cy.get('.sc-ilxdoe').click()
- cy.get('[name="name"]').type('Samuel Xavier rodrigues')
- cy.get('[name="phone"]').type('11958639439')
- cy.get('[name="email"]').type('teste@teste.com.br')
- cy.get('[name="socialId"]').type('711.867.710-89')
- cy.get('[name="dateOfBirth"]').type(21021990)
- cy.get('.sc-iKOlBD').click()
- cy.get('.sc-irLwvM').click()
- cy.contains('Prontinho! Recebemos os seus dados.').should('be.visible')
- cy.xpath("//h3/span[normalize-space()='baixe nosso app,']").click()
- 
 
-})
   
 
   
